2453da579664007b82f954d3509a84e1c38cb26f
shashang13 <shashang13@gmail.com>
2023-06-05 10:25:55 +0530

    Shared Library

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,4 +1,4 @@
-def LintChecks(){
+def lintChecks(){
   sh '''
     # Commented below line to move ahead in the project as this step is expected to fail.
     # ~/node_modules/jslint/bin/jslint.js server.js
